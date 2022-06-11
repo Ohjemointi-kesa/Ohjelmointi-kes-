307c4147574bb559e17969e340a56ddc3837fc40
Keino.Eliud <elkeino@gmail.com>
2022-06-11 07:13:59 +0300

    modified task21.c

--- task21.c
+++ task21.c
@@ -5,19 +5,24 @@ prints the result on the screen. */
 
 int main()
 {
-    int binaryNumber = 0;
-    int exponent = 1; // 2^3, 2^2, 2^1, 2^0
-    int decimal = 0;
-    int remainder = 0; // any number devide by ten  the reaminder is it's last digit
+    int binaryNumber = 0; // user input
+    int exponent = 1; // initial 2^n of 2^3, 2^2, 2^1, 2^0
+    int decimal = 0; // store calculated decimal number 
+    int remainder = 0; // the reaminder  of any number devide by ten is it's last digit
     int counter = 0;   // number of binary digit
 
     // Prompt user for binary number
     printf("Enter a five digit binary number ");
     scanf("%d", &binaryNumber);
+<<<<<<< Updated upstream
 
     int binaryValue = binaryNumber; // copy unser input to binary value
 
     // check if it is five digit binary
+=======
+    
+    // while input is greater than 0 trancate
+>>>>>>> Stashed changes
     while (binaryNumber > 0)
     {
         binaryNumber /= 10;
@@ -30,8 +35,13 @@ int main()
         return 0;
     }
 
+<<<<<<< Updated upstream
     // check that the input is not zero
     while (binaryValue != 0)
+=======
+    // while input is greater than 0 find the remainder
+    while (binaryNumber != 0)
+>>>>>>> Stashed changes
     {
 
         remainder = binaryValue % 10; // get remainder
